96e8f065d31c646630c19378f913acd49aaef0ba
Eirik W. Talberg <Eirik@Polaris.Zeus>
2016-03-05 15:50:24 +0100

    Switch to using static path variables in symlinks. Fixed git command to download submodules. Theme configuration is probably still broken.

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 touch $HOME/.zshrc
 
 #Collect submodules
-git submodule init
+git sbumodule update --init
 
 #Set up symlinks
 echo "source $HOME/.shell_config/.zshrc-local" >> $HOME/.zshrc
-ln -s .vimrc $HOME/.vimrc
-ln -s .tmux.conf $HOME/.tmux.conf 
-ln -s submodules/avit-virtualenv-zsh-theme/avit-virtualenv.zsh-theme submodules/oh-my-zsh/custom/avit-virtualenv.zsh-theme
+ln -s $HOME/.shell_config/.vimrc $HOME/.vimrc
+ln -s $HOME/.shell_config/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/.shell_config/submodules/avit-virtualenv-zsh-theme/avit-virtualenv.zsh-theme $HOME/.shell_config/submodules/oh-my-zsh/custom/avit-virtualenv.zsh-theme
 
 #Set up virtualenvs
 mkdir -p $HOME/.virtual_environments
