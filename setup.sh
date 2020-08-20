7932b8ecf8b6a4070572ed6388b3e16e6798d4e0
Eirik W. Talberg <eirikt@netcompany.com>
2020-08-20 13:29:30 +0200

    New setup script and local zsh file

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!env /bin/sh
+
+# OS X Install file
+
+#Set the proper WD
+cd 
+
+# Get oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Set ZSH file
+ln -s -f ~/.shell_config/.zshrc
+
+# Setup Tmux
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f ~/.tmux/.tmux.conf
