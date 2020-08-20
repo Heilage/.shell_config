8af155c7f5636ad9e94a2a8778f703360de1fea2
Eirik W. Talberg <eirikt@netcompany.com>
2020-08-20 13:37:48 +0200

    Added powerline theme

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ cd
 
 # Get oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 # Set ZSH file
 ln -s -f ~/.shell_config/.zshrc
 
